384447532df5e0efcf60422945db5384d6750c6e
S-Y-A-N <syanays.git@gmail.com>
2025-11-17 02:47:58 +0300

    Create bench script to compute inference speed & model size

---
+++ evaluation/bench.py
@@ -0,0 +1,109 @@
+# Inference Speed (Throughput [tokens/sec], Latency [sec/token]) and VRAM usage
+
+import argparse
+import torch
+import time
+import numpy as np
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+
+def parse_torch_dtype(dtype_str):
+    # remove "torch." prefix if present
+    name = dtype_str.split(".")[-1]
+    return getattr(torch, name)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="bench",
+        description="%(prog)s is a command-line interface used to quickly benchmark a model's size and inference speed.",
+    )
+
+    parser.add_argument(
+        "model_config_file",
+        help="- Specify a model's configuration YAML file (must contain 'name').",
+    )
+
+    args = parser.parse_args()
+    print(args)
+
+    if args.model_config_file:
+        import yaml
+
+        with open(args.model_config_file) as f:
+            model_config = yaml.safe_load(f)
+
+    model_name = model_config["name"]
+    torch_dtype = parse_torch_dtype(model_config["torch_dtype"])
+    bench(model_name, torch_dtype)
+
+
+def bench(model_name, torch_dtype):
+    device = "cuda"
+    torch.cuda.reset_peak_memory_stats(device)
+
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name, dtype=torch_dtype, device_map=device, trust_remote_code=True
+    )
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    torch.cuda.synchronize()
+
+    # VRAM usage
+    vram_bytes = torch.cuda.max_memory_allocated(device)
+    vram_gb = vram_bytes / (1024**3)
+    print(f"Model VRAM usage: {vram_gb:.3f} GiB")
+
+    prompts = [
+        "Write a short story about a girl who overcame the shackles of sickness and achieved her dream of becoming a teacher.",
+        "Write a python function that determines whether a number is prime or not.",
+        "Solve the equation 3x² - 5x + 2 = 0 and explain each step clearly.",
+        "List and explain the most common web security attacks.",
+        "Breifly explain the probability of finding the correct passcode if the passcode consists of 6 digits ranging from 0 to 9.",
+        "اكتب قصة قصيرة عن فتاة تمكنت من التغلب على قيود المرض وحققت حلمها لتصبح معلمة.",
+        "اكتب دالة بايثون تحدد ما إذا كان الرقم أولياً أم لا.",
+        "حل المعادلة 3x² - 5x + 2 = 0 واشرح كل خطوة بوضوح.",
+        "اذكر واشرح أكثر الهجمات شيوعاً على أمن الويب.",
+        "اشرح باختصار احتمال العثور على رمز مرور الصحيح إذا كان الرمز مكونًا من 6 أرقام تتراوح من 0 إلى 9.",
+    ]
+
+    elapsed, throughput, latency = [], [], []
+    GEN_TOKENS = 1024
+    for prompt in prompts:
+        inputs = tokenizer(prompt, return_tensors="pt").to(device)
+
+        # warmup (ensures GPU kernels are initialized)
+        # TODO fix jais model error: past_length = past_key_values[0][0].size(-2) AttributeError: 'NoneType' object has no attribute 'size'
+        outputs = model.generate(**inputs, max_new_tokens=64)
+        torch.cuda.synchronize()
+
+        start = time.time()
+        outputs = model.generate(
+            **inputs, max_new_tokens=GEN_TOKENS, use_cache=model.config.use_cache
+        )
+        torch.cuda.synchronize()
+        end = time.time()
+
+        elapsed.append(end - start)
+        throughput.append(GEN_TOKENS / elapsed[-1])
+        latency.append(elapsed[-1] / GEN_TOKENS)
+
+        print(f"Generated {GEN_TOKENS} tokens in {elapsed[-1]:.3f} sec")
+        print(f"Tokens/sec: {throughput[-1]:.2f}")
+        print(f"Latency (sec/token): {latency[-1]:.6f}")
+
+        text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        print("\n\nModel Output:", text)
+
+    mean_tps = np.mean(throughput)
+    std_tps = np.std(throughput)
+
+    mean_spt = np.mean(latency)
+    std_spt = np.std(latency)
+
+    print(f"\nAverage Throughput: {mean_tps:.2f} ± {std_tps:.2f} (tokens/sec)")
+    print(f"\nAverage Latency: {mean_spt:.2f} ± {std_spt:.2f} (sec/token)")
+
+
+if __name__ == "__main__":
+    main()
--- evaluation/graph.py
+++ evaluation/graph.py
@@ -8,59 +8,55 @@ filename = "results_2025-11-15T22-41-45.271369.json"
 
 json_file = "/".join([base_path, model_name, filename])
 
-results_json = load_dataset(
-  "json",
-  data_files=json_file,
-  split="train"
-)
+results_json = load_dataset("json", data_files=json_file, split="train")
 
 benchmark_labels = {
-  'arabic_mmlu:_average|0': 'Arabic MMLU',
-  'arabic_exams|0': 'Arabic EXAMS',
-  'acva:_average|0': 'AVCA',
-  'aratrust:_average|0': 'AraTrust',
-  'madinah_qa:_average|0': 'MadinahQA'
+    "arabic_mmlu:_average|0": "Arabic MMLU",
+    "arabic_exams|0": "Arabic EXAMS",
+    "acva:_average|0": "AVCA",
+    "aratrust:_average|0": "AraTrust",
+    "madinah_qa:_average|0": "MadinahQA",
 }
 
-tasks = dict.fromkeys([
-  'arabic_mmlu:_average|0',
-  'arabic_exams|0',
-  'acva:_average|0',
-  'aratrust:_average|0',
-  'madinah_qa:_average|0'
-])
+tasks = dict.fromkeys(
+    [
+        "arabic_mmlu:_average|0",
+        "arabic_exams|0",
+        "acva:_average|0",
+        "aratrust:_average|0",
+        "madinah_qa:_average|0",
+    ]
+)
 
 for task in tasks:
-  tasks[task] = float(next(iter(results_json['results'][task][0].values()))) * 100
-  print(f"{task:<25} {tasks[task]}")
+    tasks[task] = float(next(iter(results_json["results"][task][0].values()))) * 100
+    print(f"{task:<25} {tasks[task]}")
 
 
 # Plotting bar chart
-matplotlib.use('agg')
-plt.style.use('grayscale')
+matplotlib.use("agg")
+plt.style.use("grayscale")
 
 fig = plt.figure()
 num_bars = range(len(tasks))
 scores = list(tasks.values())
 xtick_labels = [benchmark_labels[k] for k in tasks.keys()]
 
-plt.bar(num_bars, scores, align='center', width=0.3)
-plt.xticks(num_bars, xtick_labels, ha='center', rotation=0)
-plt.ylim(0, 100) 
+plt.bar(num_bars, scores, align="center", width=0.3)
+plt.xticks(num_bars, xtick_labels, ha="center", rotation=0)
+plt.ylim(0, 100)
 # Source - https://stackoverflow.com/a/53073502
 # Posted by Maroca, modified by community. See post 'Timeline' for change history
 # Retrieved 2025-11-16, License - CC BY-SA 4.0
 
 for i, score in enumerate(scores):
-    plt.text(i, score + 1, f"{score:.2f}", ha='center')
+    plt.text(i, score + 1, f"{score:.2f}", ha="center")
 
 
-plt.title("Arabic Benchmark Scores", fontweight='bold')
-plt.xlabel("Benchmark", fontweight='bold')
-plt.ylabel("Score", fontweight='bold')
+plt.title("Arabic Benchmark Scores", fontweight="bold")
+plt.xlabel("Benchmark", fontweight="bold")
+plt.ylabel("Score", fontweight="bold")
 plt.tight_layout()
 
-model_name = model_name.replace(r'/', '__')
-plt.savefig(f'evaluation/figures/{model_name}.png', dpi=fig.dpi)
-
-
+model_name = model_name.replace(r"/", "__")
+plt.savefig(f"evaluation/figures/{model_name}.png", dpi=fig.dpi)
