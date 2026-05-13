d6d21c8d38f16fba25ca95727c3ec603b61c4926
SYAN <syanays.git@gmail.com>
2026-05-13 17:12:25 +0300

    add framwork summary and nav

--- docs/index.js
+++ docs/index.js
@@ -29,3 +29,30 @@ function toggleTheme() {
   document.getElementById("light-icon").classList.toggle("hidden")
   document.getElementById("dark-icon").classList.toggle("hidden")
 }
+
+// Scroll effect
+function scroll(element) {  
+  const node = document.getElementById(element);
+  if (node !== null) {
+    setTimeout(function () {
+      const headerHeight = document.getElementsByTagName('nav')[0].offsetHeight;
+      const nodeHeight = node.offsetTop;
+
+      window.scrollTo({
+        top: nodeHeight - headerHeight - 15,
+        left: 0,
+        behavior: "smooth",
+      });
+
+    }, 100);
+  }
+}
+
+const pageLinks = document.querySelectorAll('nav a');
+Array.from(pageLinks).forEach((link) => {
+  let id = link.href.split('#')[1];
+  link.addEventListener('click', (e) => {
+    e.preventDefault()
+    scroll(id);
+  });
+});
